7643ef63a92d6b534339cefff671ee6ba967d68a
Vladislav <vladislav.a.koltovicha@gmail.com>
2025-04-02 03:14:58 +0300

    Добавлено: Верстка страницы авторизации

---
+++ KawaiiList/Models/AnimeModel.cs
@@ -0,0 +1,47 @@
+﻿namespace KawaiiList.Models
+{
+    public class AnimeItem
+    {
+        public string? Ru { get; set; }
+    }
+
+    public class AnimePoster
+    {
+        private string? _url;
+
+        public string? Url
+        {
+            get => _url;
+            set => _url = value != null ? "https://www.anilibria.tv" + value : null;
+        }
+    }
+
+    public class AnimePosters
+    {
+        public AnimePoster? Original { get; set; }
+        public AnimePoster? Medium { get; set; }
+        public AnimePoster? Small { get; set; }
+    }
+
+    public class AnimeType
+    {
+        public string? String { get; set; }
+        public int? Year { get; set; }
+    }
+
+    public class Anime
+    {
+        public int Id { get; set; }
+        public AnimeItem? Names { get; set; }
+        public AnimePosters? Posters { get; set; }
+        public List<string>? Genres { get; set; }
+        public AnimeType? Season { get; set; }
+
+        public string GenresText => string.Join(" ", Genres);
+    }
+
+    public class AnimeResponse
+    {
+        public List<Anime>? List { get; set; }
+    }
+}
