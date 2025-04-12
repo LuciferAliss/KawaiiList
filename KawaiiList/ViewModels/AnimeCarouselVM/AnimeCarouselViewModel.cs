b3ef1ab636eb2141a383ca6a27d7b9543c8c8f2c
Vladislav <vladislav.a.koltovicha@gmail.com>
2025-04-12 20:10:07 +0300

    Добавление: Добавлены стили карусели

--- KawaiiList/ViewModels/AnimeCarouselVM/AnimeCarouselViewModel.cs
+++ KawaiiList/ViewModels/AnimeCarouselVM/AnimeCarouselViewModel.cs
@@ -1,11 +1,15 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using HandyControl.Controls;
 using KawaiiList.Models.Anilibria;
 using KawaiiList.Services.API;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace KawaiiList.ViewModels.AnimeCarouselVM
 {
@@ -14,18 +18,24 @@ namespace KawaiiList.ViewModels.AnimeCarouselVM
         private readonly IApiService _apiService;
 
         [ObservableProperty]
-        List<AnimeTitle>? _animeTitle;
+        ObservableCollection<AnimeTitle>? _animeTitle;
 
+        [ObservableProperty]
+        DependencyProperty _indexPage;
+
+        int _indexPageValue = 4;
 
         public AnimeCarouselViewModel(IApiService apiService)
         {
+            IndexPage = DependencyProperty.Register("PageIndex", typeof(int), typeof(Carousel), new PropertyMetadata(_indexPageValue));
             _apiService = apiService;
             _ = LoadAnime();
         }
 
         private async Task LoadAnime()
         {
-            AnimeTitle = await _apiService.GetTitlesAsync(15);
+            List<AnimeTitle> data = await _apiService.GetTitlesAsync(15);
+            AnimeTitle = [.. data];
         }
     }
 }
