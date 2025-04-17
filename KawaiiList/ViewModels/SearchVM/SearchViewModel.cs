e65412f35789f99a218545fa4b029241306552f1
Vladislav <vladislav.a.koltovicha@gmail.com>
2025-04-17 20:07:00 +0300

    Добавлено: Метод поиска

--- KawaiiList/ViewModels/SearchVM/SearchViewModel.cs
+++ KawaiiList/ViewModels/SearchVM/SearchViewModel.cs
@@ -1,15 +1,22 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using KawaiiList.Models.Anilibria;
 using KawaiiList.Services.API;
 using System.Diagnostics;
+using System.DirectoryServices;
+using System.Xml.Linq;
 
 namespace KawaiiList.ViewModels.SearchVM
 {
     public partial class SearchViewModel : ObservableObject, ISearchViewModel
     {
         private readonly IApiService _apiService;
+        private CancellationTokenSource _cts = new();
 
         [ObservableProperty]
         string _searchText;
+
+        [ObservableProperty]
+        List<AnimeTitle> _animeList = [];
 
         public SearchViewModel(IApiService apiService)
         {
@@ -19,7 +26,33 @@ namespace KawaiiList.ViewModels.SearchVM
 
         partial void OnSearchTextChanged(string value)
         {
-            Debug.WriteLine(value);
+            _cts.Cancel();
+            _cts.Dispose();
+            _cts = new CancellationTokenSource();
+            var token = _cts.Token;
+
+            AnimeList = [];
+
+            _ = Task.Run(async () =>
+            {
+                try
+                {
+                    var result = await _apiService.SearchTitlesAsync(value, token);
+
+                    if (token.IsCancellationRequested)
+                        return;
+
+                    AnimeList = result ?? new List<AnimeTitle>();
+                }
+                catch (OperationCanceledException)
+                {
+                }
+            });
+
+            if(AnimeList.Count > 0)
+            {
+                Console.WriteLine(AnimeList.Count);
+            }
         }
     }
 }
